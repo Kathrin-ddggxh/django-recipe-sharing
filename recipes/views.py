cbc5c7f44e54828907a4b4c9bd914e818ac3deef
Kay-ddggxh <kathrin.welfare@gmail.com>
2023-09-18 16:46:28 +0100

    add search functionality to recipes page

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,10 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.views.generic import CreateView, ListView, DetailView, DeleteView, UpdateView
 
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+
+from django.db.models import Q
 
 from .models import Recipe
 from .forms import RecipeForm
@@ -12,6 +16,19 @@ class Recipes(ListView):
     template_name = "recipes/recipes.html"
     model = Recipe
     context_object_name = "recipes"
+
+    def get_queryset(self, **kwargs):
+        query = self.request.GET.get("q")
+        if query: 
+            recipes = self.model.objects.filter(
+                Q(title__icontains=query) |
+                Q(description__icontains=query) |
+                Q(instructions__icontains=query) |
+                Q(cuisine_types__icontains=query)
+            )
+        else:
+            recipes = self.model.objects.all()
+        return recipes
 
 
 class RecipeDetail(DetailView):
