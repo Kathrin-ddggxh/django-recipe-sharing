91971b849861a62d4e99508235c39806ade29c5b
Kay-ddggxh <kathrin.welfare@gmail.com>
2023-09-18 17:19:10 +0100

    create list view and styling for home page

--- home/views.py
+++ home/views.py
@@ -1,7 +1,15 @@
-from django.views.generic import TemplateView
+from typing import Any
+from django.db.models.query import QuerySet
+from django.views.generic import TemplateView, ListView
+from recipes.models import Recipe
 
 
-class Index(TemplateView):
+class Index(ListView):
     template_name = "home/index.html"
+    model = Recipe
+    context_object_name = "recipes"
+
+    def get_queryset(self):
+        return self.model.objects.all()[:3]
 
 
